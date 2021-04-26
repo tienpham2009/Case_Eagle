94052ae03b1a82cbbb038157055e0fa21bd8a1ab
tienpham2009 <tiendx2009@gmail.com>
2021-04-26 10:33:55 +0700

    dai bang

---
+++ myjs/rectangle.js
@@ -0,0 +1,12 @@
+class Rectangle{
+    constructor(x,y,width,height) {
+        this.rectX = x;
+        this.rectY = y;
+        this.rectWidth = width;
+        this.rectHeight = height;
+    }
+
+    draw(){
+
+    }
+}
